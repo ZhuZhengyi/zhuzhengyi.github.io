27ce16abe092c365797e14cc8440742b40763f4e
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2022-04-26 07:34:09 +0000

    deploy: ZhuZhengyi/gitnote@dbe56d0e010b0b38ba3dc044b1d198f3664b8640

---
+++ js/l2d.js
@@ -0,0 +1,22 @@
+L2Dwidget.init({
+    "model": {
+        jsonPath: "https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json",
+        "scale": 1
+    },
+    "display": {
+        "position": "left",
+        "width": 100,
+        "height": 200,
+        "hOffset": 10,
+        "vOffset": -20
+    },
+    "mobile": {
+        "show": true,
+        "scale": 0.5
+    },
+    "react": {
+        "opacityDefault": 0.7,
+        "opacityOnHover": 0.2
+    }
+});
+
