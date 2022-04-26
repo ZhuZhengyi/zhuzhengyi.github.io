a40816cbd2591f3e9cef245fb1cfdd5e2b23bb65
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2022-04-26 07:23:55 +0000

    deploy: ZhuZhengyi/gitnote@fe8a15e10522fabc2e1c46ed0d09fca2b3632e0d

---
+++ post/leetcode/cpp/60.排列序列.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=60 lang=cpp
+ *
+ * [60] 排列序列
+ *
+ * https://leetcode-cn.com/problems/permutation-sequence/description/
+ *
+ * algorithms
+ * Hard (52.96%)
+ * Likes:    659
+ * Dislikes: 0
+ * Total Accepted:    105.1K
+ * Total Submissions: 198.4K
+ * Testcase Example:  '3\n3'
+ *
+ * 给出集合 [1,2,3,...,n]，其所有元素共有 n! 种排列。
+ * 
+ * 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+ * 
+ * 
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 
+ * 
+ * 给定 n 和 k，返回第 k 个排列。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 3, k = 3
+ * 输出："213"
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 4, k = 9
+ * 输出："2314"
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：n = 3, k = 1
+ * 输出："123"
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 1 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+
+    }
+};
+// @lc code=end
+
