f8071637f68f75958b553c70c27c0e2b694dd169
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2022-04-27 02:51:27 +0000

    deploy: ZhuZhengyi/gitnote@039923c684e51c5498fbb5a8dec81d771dbb3578

--- post/leetcode/cpp/60.排列序列.cpp
+++ post/leetcode/cpp/60.排列序列.cpp
@@ -62,11 +62,44 @@
  * 
  */
 
-// @lc code=start
+#include <string>
+
+using namespace std;
+
+// @lc code=start 
 class Solution {
 public:
+    /*
+    * ## 解题思路
+    * * 数学推导
+    * 1. n个字符的全排列总数为n!;
+    * 2. 以任意一个字符为首字符的排列个数均(n-1)!；
+    * 3. 因此，k = c1 * (n-1)! + x, 其中，c1为首字符；
+    *    则第一个字符 c1 = [k / (n-1)!]， []为下取底；
+    *    f(n, k) = c1 + f(n-1, k-1)
+    * 4. 解题时，可以用一个字母表"123456789"来记录待取的字符；
+    * 5. 将1..k转换为0..(k-1), 可以和上面的字母表统一，且能很好的计算
+    */
     string getPermutation(int n, int k) {
+        string letters = string("123456789").substr(0, n);
+        string result = "";
+        --k;  //将1..k转化为0..(k-1)
+        while (k>0) {
+            int i = k / getFactor(n-1) ;
+            result.push_back(letters[i]);
+            letters.erase(letters.begin()+i);
+            k %= getFactor(n-1);
+            n--;
+        }
 
+        return result+letters;
+    }
+
+    int getFactor(int n) {
+        if (n==0) {
+            return 1;
+        }
+        return n*getFactor(n-1);
     }
 };
 // @lc code=end
