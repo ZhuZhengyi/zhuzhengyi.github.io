b59cb6e65ba7ff0ffa1a8dea28da685f97e8d0e6
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2022-04-27 01:16:01 +0000

    deploy: ZhuZhengyi/gitnote@0cc0ab19e42b098e863d407f9910d7783d40c38f

--- js/l2d/L2Dwidget.init.js
+++ js/l2d/L2Dwidget.init.js
@@ -3,7 +3,7 @@ L2Dwidget.init({
         jsonPath: "https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json",
         hHeadPos: 0.5,
         vHeadPos: 0.618,
-        scale: 1
+        scale: 0.8
     },
     display: {
         position: "right",
@@ -11,7 +11,7 @@ L2Dwidget.init({
         width: 100,
         height: 200,
         hOffset: 10,
-        vOffset: 100
+        vOffset: 10
     },
     mobile: {
         show: false,
