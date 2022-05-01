10c53de03a4bced7ed41a16761c82bb651614183
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2022-05-01 08:34:52 +0000

    deploy: ZhuZhengyi/gitnote@313b082e3565411ca161fe95fcbe14489e766f11

---
+++ post/leetcode/cpp/50.pow-x-n.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ *
+ * https://leetcode-cn.com/problems/powx-n/description/
+ *
+ * algorithms
+ * Medium (37.83%)
+ * Likes:    944
+ * Dislikes: 0
+ * Total Accepted:    283.4K
+ * Total Submissions: 749.2K
+ * Testcase Example:  '2.00000\n10'
+ *
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，x^n^ ）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：x = 2.00000, n = 10
+ * 输出：1024.00000
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：x = 2.10000, n = 3
+ * 输出：9.26100
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：x = 2.00000, n = -2
+ * 输出：0.25000
+ * 解释：2^-2 = 1/2^2 = 1/4 = 0.25
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * -100.0 < x < 100.0
+ * -2^31 <= n <= 2^31-1
+ * -10^4 <= x^n <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+    }
+};
+// @lc code=end
+
