ac68ad033725f8d5e97374abea0254b33a57919d
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2022-11-09 07:23:41 +0000

    deploy: ZhuZhengyi/gitnote@c5b9c20cbaf34c0205adfba38eebdc2ca7dde2e4

--- post/leetcode/python/two-sum.py
+++ post/leetcode/python/two-sum.py
@@ -1,25 +1,53 @@
-# coding:utf-8
-'''
-@Copyright:LintCode
-@Author:   justice_103
-@Problem:  http://www.lintcode.com/problem/two-sum
-@Language: Python
-@Datetime: 15-08-05 12:50
-'''
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+# https://leetcode-cn.com/problems/two-sum/description/
+#
+# algorithms
+# Easy (47.64%)
+# Likes:    7740
+# Dislikes: 0
+# Total Accepted:    854.8K
+# Total Submissions: 1.8M
+# Testcase Example:  '[2,7,11,15]\n9'
+#
+# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+# 
+# 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+# 
+# 示例:
+# 
+# 给定 nums = [2, 7, 11, 15], target = 9
+# 
+# 因为 nums[0] + nums[1] = 2 + 7 = 9
+# 所以返回 [0, 1]
+# 
+# 
+#
+from typing import List
 
+# @lc code=start
 class Solution:
-    """
-    @param numbers : An array of Integer
-    @param target : target = numbers[index1] + numbers[index2]
-    @return : [index1 + 1, index2 + 1] (index1 < index2)
-    """
-    def twoSum(self, numbers, target):
-        # write your code here
-        n=len(numbers)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        ## 解题思路：
+        * map
+        1. 用一个map记录遍历过的数字, map为(target-n) : id
+        2. 遍历时如果map命中, 则表示已找到。
+        '''
+        recorded = {}
+        for i, num in enumerate(nums):
+            j = recorded.get(num, None)
+            if j is not None:
+                return [j, i] 
+            else:
+                recorded[target-num] = i
+        return []
+# @lc code=end
 
-        for i in range(n):
-            for j in range(i+1, n):
-                if numbers[i]+numbers[j]==target:
-                    return [i+1,j+1]
+if __name__ == "__main__":
+    s = Solution()
+    print(s.twoSum([2, 7, 11, 15], 9))
 
-        return None
---
+++ post/leetcode/rust/src/solution/695.max-area-of-island.rs
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=695 lang=rust
+ *
+ * [695] 岛屿的最大面积
+ *
+ * https://leetcode.cn/problems/max-area-of-island/description/
+ *
+ * algorithms
+ * Medium (67.92%)
+ * Total Accepted:    249K
+ * Total Submissions: 366.6K
+ * Testcase Example:  '[[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]'
+ *
+ * 给你一个大小为 m x n 的二进制矩阵 grid 。
+ * 
+ * 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。你可以假设 grid
+ * 的四个边缘都被 0（代表水）包围着。
+ * 
+ * 岛屿的面积是岛上值为 1 的单元格的数目。
+ * 
+ * 计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：grid =
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 输出：6
+ * 解释：答案不应该是 11 ，因为岛屿只能包含水平或垂直这四个方向上的 1 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：grid = [[0,0,0,0,0,0,0,0]]
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 50
+ * grid[i][j] 为 0 或 1
+ * 
+ * 
+ */
+impl Solution {
+    /// ## 解题思路
+    /// * 深度优先搜索
+    pub fn max_area_of_island(grid: Vec<Vec<i32>>) -> i32 {
+        let (mut stack, mut max_area) = (vec![], 0);
+        let mut visited = vec![vec![false; grid[0].len()]; grid.len()];
+        for y in 0..grid.len() {  
+            for x in 0..grid[y].len() {
+                let mut area = 0;
+                stack.push((x, y));
+                while let Some((x, y)) = stack.pop() {
+                    if grid[y][x] != 0 && !visited[y][x] {
+                        area += 1;
+                        visited[y][x] = true;
+                        [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                            .iter()
+                            .map(|(dx, dy)| ( (x as isize + dx) as usize, (y as isize + dy) as usize ))
+                            .filter(|&(x, y)| y < grid.len() && x < grid[y].len() && grid[y][x] != 0 && !visited[y][x])
+                            .for_each(|(x, y)| stack.push((x, y)));
+                    }
+                }
+                max_area = max_area.max(area);
+            }
+        }
+
+        max_area
+    } 
+}
