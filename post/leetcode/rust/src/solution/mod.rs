d2c7209485bc4523dfb917d2fedad57c735ac1f3
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2023-02-13 11:50:06 +0000

    deploy: ZhuZhengyi/gitnote@2c004debff2bf97674d8937c344dc334c3b2882c

--- post/leetcode/rust/src/solution/mod.rs
+++ post/leetcode/rust/src/solution/mod.rs
@@ -1,23 +1,25 @@
 // solution.rs
 //
 
-#[path="1.两数之和.rs"]
+#[path = "1.两数之和.rs"]
 mod _0001_two_sum;
 
-#[path="./10.正则表达式匹配.rs"]
+#[path = "./10.正则表达式匹配.rs"]
 mod _0010_is_match;
+
+#[path = "./878.第-n-个神奇数字.rs"]
+mod _0878_is_match;
 
 // #[path="./102.二叉树的层序遍历.rs"]
 // mod _0102_bt_level_travel;
 
-
 /// Solution
-pub struct Solution{}
+pub struct Solution {}
 
 impl Solution {
     /// new a Solution
     pub(crate) fn new() -> Self {
-        Solution{}
+        Solution {}
     }
 }
 
@@ -26,4 +28,3 @@ impl Default for Solution {
         Self::new()
     }
 }
-
