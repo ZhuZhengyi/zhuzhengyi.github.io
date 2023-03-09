53c934690070b0ea99f73e8c26947cd51cf4bc4a
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2023-03-09 11:43:57 +0000

    deploy: ZhuZhengyi/gitnote@54382fc5697343f63b89d92a45376451f08cf231

--- post/leetcode/rust/src/solution/173.二叉搜索树迭代器.rs
+++ post/leetcode/rust/src/solution/173.二叉搜索树迭代器.rs
@@ -1,80 +1,80 @@
 /*
- * @lc app=leetcode.cn id=173 lang=rust
- *
- * [173] 二叉搜索树迭代器
- *
- * https://leetcode-cn.com/problems/binary-search-tree-iterator/description/
- *
- * algorithms
- * Medium (80.47%)
- * Likes:    520
- * Dislikes: 0
- * Total Accepted:    78K
- * Total Submissions: 96.9K
- * Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n' +
-  '[[[7,3,15,null,null,9,20]],[],[],[],[],[],[],[],[],[]]'
- *
- * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
- * 
- * 
- * 
- * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root
- * 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
- * boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
- * int next()将指针向右移动，然后返回指针处的数字。
- * 
- * 
- * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
- * 
- * 
- * 
- * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
- * 
- * 
- * 
- * 示例：
- * 
- * 
- * 输入
- * ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next",
- * "hasNext", "next", "hasNext"]
- * [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
- * 输出
- * [null, 3, 7, true, 9, true, 15, true, 20, false]
- * 
- * 解释
- * BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
- * bSTIterator.next();    // 返回 3
- * bSTIterator.next();    // 返回 7
- * bSTIterator.hasNext(); // 返回 True
- * bSTIterator.next();    // 返回 9
- * bSTIterator.hasNext(); // 返回 True
- * bSTIterator.next();    // 返回 15
- * bSTIterator.hasNext(); // 返回 True
- * bSTIterator.next();    // 返回 20
- * bSTIterator.hasNext(); // 返回 False
- * 
- * 
- * 
- * 
- * 提示：
- * 
- * 
- * 树中节点的数目在范围 [1, 10^5] 内
- * 0 
- * 最多调用 10^5 次 hasNext 和 next 操作
- * 
- * 
- * 
- * 
- * 进阶：
- * 
- * 
- * 你可以设计一个满足下述条件的解决方案吗？next() 和 hasNext() 操作均摊时间复杂度为 O(1) ，并使用 O(h) 内存。其中 h
- * 是树的高度。
- * 
- * 
- */
+* @lc app=leetcode.cn id=173 lang=rust
+*
+* [173] 二叉搜索树迭代器
+*
+* https://leetcode-cn.com/problems/binary-search-tree-iterator/description/
+*
+* algorithms
+* Medium (80.47%)
+* Likes:    520
+* Dislikes: 0
+* Total Accepted:    78K
+* Total Submissions: 96.9K
+* Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n' +
+ '[[[7,3,15,null,null,9,20]],[],[],[],[],[],[],[],[],[]]'
+*
+* 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+*
+*
+*
+* BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root
+* 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+* boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+* int next()将指针向右移动，然后返回指针处的数字。
+*
+*
+* 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+*
+*
+*
+* 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+*
+*
+*
+* 示例：
+*
+*
+* 输入
+* ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next",
+* "hasNext", "next", "hasNext"]
+* [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+* 输出
+* [null, 3, 7, true, 9, true, 15, true, 20, false]
+*
+* 解释
+* BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+* bSTIterator.next();    // 返回 3
+* bSTIterator.next();    // 返回 7
+* bSTIterator.hasNext(); // 返回 True
+* bSTIterator.next();    // 返回 9
+* bSTIterator.hasNext(); // 返回 True
+* bSTIterator.next();    // 返回 15
+* bSTIterator.hasNext(); // 返回 True
+* bSTIterator.next();    // 返回 20
+* bSTIterator.hasNext(); // 返回 False
+*
+*
+*
+*
+* 提示：
+*
+*
+* 树中节点的数目在范围 [1, 10^5] 内
+* 0
+* 最多调用 10^5 次 hasNext 和 next 操作
+*
+*
+*
+*
+* 进阶：
+*
+*
+* 你可以设计一个满足下述条件的解决方案吗？next() 和 hasNext() 操作均摊时间复杂度为 O(1) ，并使用 O(h) 内存。其中 h
+* 是树的高度。
+*
+*
+*/
 
 use super::*;
 
@@ -98,9 +98,8 @@ use super::*;
 //   }
 // }
 
-use std::rc::Rc;
 use std::cell::RefCell;
-
+use std::rc::Rc;
 
 #[derive(Clone)]
 enum BSTIteratorState {
@@ -118,17 +117,19 @@ enum BSTIteratorState {
 impl BSTIterator {
     /// ## 解题思路
     fn new(root: Option<Rc<RefCell<TreeNode>>>) -> Self {
-        BSTIterator { 
+        BSTIterator {
             root,
             state: BSTIteratorState::New,
         }
     }
-    
+
     fn next(&mut self) -> i32 {
         match self.state.clone() {
             BSTIteratorState::New => {
                 if self.root.is_some() {
-                    self.state = BSTIteratorState::Left(Rc::new(RefCell::new(BSTIterator::new(self.root.as_ref().unwrap().borrow().left.clone()))));
+                    self.state = BSTIteratorState::Left(Rc::new(RefCell::new(BSTIterator::new(
+                        self.root.as_ref().unwrap().borrow().left.clone(),
+                    ))));
                 } else {
                     self.state = BSTIteratorState::Finished;
                 }
@@ -136,16 +137,17 @@ impl BSTIterator {
             }
             BSTIteratorState::Left(iter) => {
                 if iter.borrow_mut().next() != -1 {
-                    iter.borrow_mut().next()   
+                    iter.borrow_mut().next()
                 } else {
                     self.state = BSTIteratorState::Val;
                     self.next()
                 }
             }
             BSTIteratorState::Val => {
-                self.state = BSTIteratorState::Right(Rc::new(RefCell::new(BSTIterator::new(self.root.as_ref().unwrap().borrow().right.clone()))));
+                self.state = BSTIteratorState::Right(Rc::new(RefCell::new(BSTIterator::new(
+                    self.root.as_ref().unwrap().borrow().right.clone(),
+                ))));
                 self.root.clone().unwrap().borrow().val
-
             }
             BSTIteratorState::Right(iter) => {
                 if iter.borrow_mut().next() != -1 {
@@ -154,14 +156,11 @@ impl BSTIterator {
                     self.state = BSTIteratorState::Finished;
                     self.next()
                 }
-
             }
-            BSTIteratorState::Finished => {
-                -1
-            }
+            BSTIteratorState::Finished => -1,
         }
     }
-    
+
     fn has_next(&self) -> bool {
         match self.state {
             BSTIteratorState::Finished => false,
@@ -177,8 +176,7 @@ impl BSTIterator {
  * let ret_2: bool = obj.has_next();
  */
 struct BSTIterator {
-    state: BSTIteratorState, 
     root: Option<Rc<RefCell<TreeNode>>>,
+    state: BSTIteratorState,
 }
 // @lc code=end
-
