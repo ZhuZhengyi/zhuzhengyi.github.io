bf22552803a678355a721932359c143b8d7cc753
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2023-03-15 06:34:52 +0000

    deploy: ZhuZhengyi/gitnote@f868f098b664b070fc4630bce0917da579d2c4cf

---
+++ post/leetcode/rust/src/solution/215.数组中的第k个最大元素.rs
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=rust
+ *
+ * [215] 数组中的第K个最大元素
+ *
+ * https://leetcode.cn/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (64.03%)
+ * Likes:    2092
+ * Dislikes: 0
+ * Total Accepted:    825.4K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+ *
+ * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ *
+ * 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: [3,2,1,5,6,4], k = 2
+ * 输出: 5
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: [3,2,3,1,2,4,5,5,6], k = 4
+ * 输出: 4
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= k <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+impl Solution {
+    /// ## 解题思路
+    pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+        todo!()
+    }
+}
+// @lc code=end
+
+struct Solution;
+
+mod tests {
+
+    fn test() {
+
+    }
+
+}
