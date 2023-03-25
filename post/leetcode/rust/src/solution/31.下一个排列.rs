cf4e444122302317b2e75d9bf85f0e3044b63a12
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2023-03-25 13:37:06 +0000

    deploy: ZhuZhengyi/gitnote@b49b9fe9965cd782659198a11207b8cb1195d34b

--- post/leetcode/rust/src/solution/31.下一个排列.rs
+++ post/leetcode/rust/src/solution/31.下一个排列.rs
@@ -14,87 +14,94 @@
  * Testcase Example:  '[1,2,3]'
  *
  * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
- * 
+ *
  * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
- * 
+ *
  * 必须 原地 修改，只允许使用额外常数空间。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：nums = [1,2,3]
  * 输出：[1,3,2]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：nums = [3,2,1]
  * 输出：[1,2,3]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：nums = [1,1,5]
  * 输出：[1,5,1]
- * 
- * 
+ *
+ *
  * 示例 4：
- * 
- * 
+ *
+ *
  * 输入：nums = [1]
  * 输出：[1]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= nums.length <= 100
  * 0 <= nums[i] <= 100
- * 
- * 
+ *
+ *
  */
 
-struct Solution;
 
 // @lc code=start
 impl Solution {
-    /// 思路：
-    /// 1. 从右往左，找到第一个小于其右边的数，将两数交换；
-    /// 2. 交换后，将该数右侧的子序列重新从左到右增序排列；
+    /// ## 解题思路：
+    /// - 交换
+    /// 1. 从右往左，找到第一个nums[i-1]<nums[i]的i;
+    /// 2. 然后再从[i+1..]中查找比nums[i-1]大的最小数nums[j];
+    /// 3. 交换nums[i-1],nums[j];
+    /// 4. 交换后，重新按从小到大排列nums[i..]；
     pub fn next_permutation(nums: &mut Vec<i32>) {
-        let l = nums.len();
-        if l < 2 {
-            return
+        let len = nums.len();
+        if len < 2 {
+            return;
         }
 
-        let mut i = l-1;
-        while i > 0 {
-            // 从右向左，找到第一个小于其右侧元素的元素
-            if nums[i-1] < nums[i] {
-                let mut bigger = i;
-                for j in i+1..l {
-                    //查找其右侧只比该元素大一点的元素
-                    if nums[j] > nums[i-1] && nums[j] < nums[bigger] {
-                        bigger = j;
-                    }
-                }
-                nums.swap(i-1, bigger);
+        let mut resort_i = 0;  //要重新排序的部分起始index
+        // 从右向左遍历
+        for i in (1..len).rev() {
+            // 找到第一个nums[i-1]<nums[i]
+            if nums[i - 1] < nums[i] {
+                // 在[i..]中, 查找比nums[i-1]大的最小数
+                let bigger = nums[i..]
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, &n)| n > nums[i - 1])
+                    .min_by(|(_, &n1), (_, &n2)| n1.cmp(&n2))
+                    .map(|(j, _)| j + i)
+                    .unwrap_or(i);
+                // 交换nums[i-1], nums[bigger]
+                nums.swap(i - 1, bigger);
+                resort_i = i; //标记交换点
                 break;
             }
-            i-=1;
         }
 
-        // 
-        nums[i..].sort();
+        //重新排序nums[i..]
+        nums[resort_i..].sort();
     }
 }
 // @lc code=end
+
+struct Solution;
 
 #[cfg(test)]
 mod tests {
@@ -102,8 +109,8 @@ mod tests {
 
     #[test]
     fn test() {
-        let mut nums = vec![1,2,3];
+        let mut nums = vec![1, 2, 3];
         Solution::next_permutation(&mut nums);
-        assert_eq!(nums, vec![1,3,2]);
+        assert_eq!(nums, vec![1, 3, 2]);
     }
 }
