74e8e5d16aec2a78e84d6de856bfb0270cc88966
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2023-06-22 00:37:03 +0000

    deploy: ZhuZhengyi/gitnote@50d75cfc2b90ebb36877dfb9caf019b09667c062

--- post/leetcode/rust/src/solution/208.实现-trie-前缀树.rs
+++ post/leetcode/rust/src/solution/208.实现-trie-前缀树.rs
@@ -1,82 +1,74 @@
 /*
- * @lc app=leetcode.cn id=208 lang=rust
- *
- * [208] 实现 Trie (前缀树)
- *
- * https://leetcode.cn/problems/implement-trie-prefix-tree/description/
- *
- * algorithms
- * Medium (72.01%)
- * Likes:    1412
- * Dislikes: 0
- * Total Accepted:    252.6K
- * Total Submissions: 351.1K
- * Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' +
-  '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
- *
- * Trie（发音类似 "try"）或者说 前缀树
- * 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
- * 
- * 请你实现 Trie 类：
- * 
- * 
- * Trie() 初始化前缀树对象。
- * void insert(String word) 向前缀树中插入字符串 word 。
- * boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回
- * false 。
- * boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true
- * ；否则，返回 false 。
- * 
- * 
- * 
- * 
- * 示例：
- * 
- * 
- * 输入
- * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
- * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
- * 输出
- * [null, null, true, false, true, null, true]
- * 
- * 解释
- * Trie trie = new Trie();
- * trie.insert("apple");
- * trie.search("apple");   // 返回 True
- * trie.search("app");     // 返回 False
- * trie.startsWith("app"); // 返回 True
- * trie.insert("app");
- * trie.search("app");     // 返回 True
- * 
- * 
- * 
- * 
- * 提示：
- * 
- * 
- * 1 
- * word 和 prefix 仅由小写英文字母组成
- * insert、search 和 startsWith 调用次数 总计 不超过 3 * 10^4 次
- * 
- * 
- */
+* @lc app=leetcode.cn id=208 lang=rust
+*
+* [208] 实现 Trie (前缀树)
+*
+* https://leetcode.cn/problems/implement-trie-prefix-tree/description/
+*
+* algorithms
+* Medium (72.01%)
+* Likes:    1412
+* Dislikes: 0
+* Total Accepted:    252.6K
+* Total Submissions: 351.1K
+* Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' +
+ '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+*
+* Trie（发音类似 "try"）或者说 前缀树
+* 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+*
+* 请你实现 Trie 类：
+*
+*
+* Trie() 初始化前缀树对象。
+* void insert(String word) 向前缀树中插入字符串 word 。
+* boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回
+* false 。
+* boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true
+* ；否则，返回 false 。
+*
+*
+*
+*
+* 示例：
+*
+*
+* 输入
+* ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+* [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+* 输出
+* [null, null, true, false, true, null, true]
+*
+* 解释
+* Trie trie = new Trie();
+* trie.insert("apple");
+* trie.search("apple");   // 返回 True
+* trie.search("app");     // 返回 False
+* trie.startsWith("app"); // 返回 True
+* trie.insert("app");
+* trie.search("app");     // 返回 True
+*
+*
+*
+*
+* 提示：
+*
+*
+* 1
+* word 和 prefix 仅由小写英文字母组成
+* insert、search 和 startsWith 调用次数 总计 不超过 3 * 10^4 次
+*
+*
+*/
 
 // @lc code=start
 
 use std::collections::HashMap;
 
+#[derive(Default)]
 struct TrieNode {
-    children: HashMap<char, TrieNode>,
+    chars: HashMap<char, TrieNode>,
     is_word: bool,
-}
-
-impl TrieNode {
-    fn new() -> Self {
-        TrieNode {
-            children: HashMap::new(),
-            is_word: false,
-        }
-    }
 }
 
 struct Trie {
@@ -88,26 +80,26 @@ struct Trie {
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl Trie {
-
     /// ## new
     fn new() -> Self {
-        Trie { root: TrieNode::new(), }
+        Trie {
+            root: TrieNode::default(),
+        }
     }
-    
+
     /// 插入
     fn insert(&mut self, word: String) {
         let mut node = &mut self.root;
         for c in word.chars() {
-            let next= node.children.entry(c).or_insert(TrieNode::new());
-            node = next;
+            node = node.chars.entry(c).or_insert(TrieNode::default());
         }
         node.is_word = true;
     }
-    
+
     fn search(&self, word: String) -> bool {
         let mut node = &self.root;
         for c in word.chars() {
-            if let Some(next) = node.children.get(&c) {
+            if let Some(next) = node.chars.get(&c) {
                 node = next;
             } else {
                 return false;
@@ -116,11 +108,11 @@ impl Trie {
 
         node.is_word
     }
-    
+
     fn starts_with(&self, prefix: String) -> bool {
         let mut node = &self.root;
         for c in prefix.chars() {
-            if let Some(next) = node.children.get(&c) {
+            if let Some(next) = node.chars.get(&c) {
                 node = next;
             } else {
                 return false;
