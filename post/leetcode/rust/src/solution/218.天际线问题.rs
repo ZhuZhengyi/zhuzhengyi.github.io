300b57133a037a1f72be745e2ec8563ba2dc9c7a
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2023-07-14 05:15:12 +0000

    deploy: ZhuZhengyi/gitnote@83649a9da7530263fa2a29ac65f8a764a9eb6c44

--- post/leetcode/rust/src/solution/218.天际线问题.rs
+++ post/leetcode/rust/src/solution/218.天际线问题.rs
@@ -68,50 +68,65 @@ struct Solution;
 impl Solution {
     /// ## 解题思路
     /// - 分治+归并
-    /// 1.
+    /// 1. 整体所有矩形的天际线 = merge(左半部分矩形天际线, 右半部分天际线)
+    /// 2. 如此递归, 直到左右为1个矩形, 此时天际线为:
+    ///             [
+    ///                [buildings[0][0], buildings[0][2]],
+    ///                [buildongs[0][1], buildings[0][2]]
+    ///             ];
+    /// 3. merge流程:
+    ///    待merge: left_skylines: [ [xl1, yl1], [xl2, yl2], [xl3, yl3]...]
+    ///            right_skylines: [ [xr1, yr1], [xr2, yr2], ...]
+    ///    3.1 分别设置指针(l, r), 指向左右子集的开始;
+    ///    3.2 合并时, 选择左右集中x坐标较小的作为转折点的x坐标, 对应的h中高的为y坐标;
+    ///    3.3 如果新转折点和上一个转折点的y坐标相同, 则新转折点和上一个转折点在同一个横线上, 需要忽略新转折点;
+    ///    3.4 如果新转折点和上一个转折点的x坐标相同, 则新转折点和上一个转折点在同一个竖线上, 则只需更新上一个转折点点y坐标为新的y;
+    ///    3.5 其他情况下, 直接将新转折点加入到总合并集的尾部;
     pub fn get_skyline(buildings: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        // 获取指定范围[l, r)内的天际线
-        fn get_skyline_by_range(buildings: &[Vec<i32>], l: usize, r: usize) -> Vec<Vec<i32>> {
-            if l >= r {
+        // 获取指定范围[start, end)内的天际线
+        fn get_skyline_by_range(buildings: &[Vec<i32>], start: usize, end: usize) -> Vec<Vec<i32>> {
+            if end - start < 1 {
                 return vec![];
-            } else if l + 1 == r {
+            } else if end - start == 1 {
                 return vec![
-                    vec![buildings[l][0], buildings[l][2]],
-                    vec![buildings[l][1], 0],
+                    vec![buildings[start][0], buildings[start][2]],
+                    vec![buildings[start][1], 0],
                 ];
             }
 
             // r - l > 1
             let mut res: Vec<Vec<i32>> = vec![];
-            let mid = (l + r) >> 1;
-            let left_res = get_skyline_by_range(buildings, l, mid);
-            let right_res = get_skyline_by_range(buildings, mid, r);
-            let (mut x, mut yl, mut yr) = (0, 0, 0);
-            let (mut i, mut j) = (0, 0);
-            // merge left and right skylines
-            while i < left_res.len() || j < right_res.len() {
-                // 如果
-                if (i < left_res.len() && left_res[i][0] < right_res[j][0]) || j == right_res.len()
+            let mid = (start + end) >> 1;
+            let left_res = get_skyline_by_range(buildings, start, mid);
+            let right_res = get_skyline_by_range(buildings, mid, end);
+            let (mut x, mut hl, mut hr) = (0, 0, 0);
+            let (mut l, mut r) = (0, 0);
+            // 合并 left_res, right_res
+            while l < left_res.len() || r < right_res.len() {
+                // 如果right区间合并完了或者左边区间x坐标 < 右边的x坐标
+                if r == right_res.len() || (l < left_res.len() && left_res[l][0] < right_res[r][0])
                 {
-                    x = left_res[i][0];
-                    yl = left_res[i][1];
-                    i += 1;
+                    x = left_res[l][0];
+                    hl = left_res[l][1];
+                    l += 1;
                 } else {
-                    x = right_res[j][0];
-                    yr = right_res[j][1];
-                    j += 1;
+                    x = right_res[r][0];
+                    hr = right_res[r][1];
+                    r += 1;
                 }
-                let y = yl.max(yr);
+                // 转折点高度为左右两个待合并点中高的那个
+                let h = std::cmp::max(hl, hr);
                 let len = res.len();
-                // 如果y坐标和前一个相同, 则跳过
-                if len > 0 && res[len - 1][1] == y {
+                // 如果新转折点高度和上一个转折点的相同, 则跳过
+                if len > 0 && h == res[len - 1][1] {
                     continue;
                 }
-                // 如果x坐标和前一个相同, 则更新上一个y坐标;
-                if len > 0 && res[len - 1][0] == x {
-                    res[len - 1][1] = y;
+                // 如果新转折点x坐标和上一个相同, 则更新上一个y坐标;
+                if len > 0 && x == res[len - 1][0] {
+                    res[len - 1][1] = h;
                 } else {
-                    res.push(vec![x, y]);
+                    // 否则,将(x, h)作为新的转折点, 加入到结果集中
+                    res.push(vec![x, h]);
                 }
             }
 
