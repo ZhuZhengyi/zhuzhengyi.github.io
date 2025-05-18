b852e52d44c8ae1272654d9729c2db3a2ade8542
ZhuZhengyi <ZhuZhengyi@users.noreply.github.com>
2025-05-18 14:44:57 +0000

    deploy: ZhuZhengyi/gitnote@e327b7f01e73233fa30b083d52f033345403739e

--- js/l2d/L2Dwidget.init.js
+++ js/l2d/L2Dwidget.init.js
@@ -6,21 +6,21 @@ L2Dwidget.init({
         scale: 0.7
     },
     display: {
-        position: "left",
-        superSample: 2,
-        width: 80,
-        height: 160,
-        hOffset: 10,
-        vOffset: 10
+        position: 'left',   // 显示位置：左或右
+        superSample: 9,     // 超采样等级，相当于清晰度，数值越高越清晰
+        width: 80,         // canvas的宽度
+        height: 160,        // canvas的高度
+        hOffset: 10,         // canvas水平偏移，正方向为右
+        vOffset: 10,         // canvas垂直偏移
     },
     mobile: {
-        show: false,
-        motion: true,
-        scale: 0.5
+        show: false,         // 是否在移动设备上显示
+        motion: true,       // 移动设备是否开启重力感应
+        scale: 0.5,           // 移动设备上的缩放
     },
     react: {
-        opacityDefault: 0.7,
-        opacityOnHover: 0.2
-    }
+        opacityDefault: 0.7,  // 默认透明度
+        opacityOnHover: 0.2,  // 鼠标移上透明度
+    },
 });
 
